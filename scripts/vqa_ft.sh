a047732f4bbc86dcdb0aeb25bb30fa59807f2e1b
Thomas Winterbottom <crhf63@ncc.clients.dur.ac.uk>
2022-03-28 18:10:11 +0100

    fixing

---
+++ scripts/vqa_ft.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p res-gpu-small
+#SBATCH -c 8
+#SBATCH -t 2-00:00
+#SBATCH -x gpu[0-8]
+#SBATCH --qos short
+#SBATCH --job-name ft
+#SBATCH --mem 16G
+#SBATCH --gres gpu:1
+#SBATCH -o ../result/ft.out
+
+cd ..
+source venv/bin/activate
+
+python run.py with data_root=data/vqa_arrow num_gpus=1 num_nodes=1 task_finetune_vqa_clip_bert per_gpu_batchsize=256 load_path=checkpoints/meter_clip16_224_roberta_pretrain.ckpt clip16 text_roberta image_size=224 clip_randaug 
