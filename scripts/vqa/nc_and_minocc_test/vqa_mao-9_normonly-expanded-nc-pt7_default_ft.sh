c46eb5dfeac340302283d83c3741333bf622b86f
Jumperkables <tom.winterbottom@msn.com>
2022-06-07 11:38:30 +0100

    fixed the scripts labelling sizes

--- scripts/vqa/nc_and_minocc_test/vqa_mao-9_normonly-expanded-nc-pt7_default_ft.sh
+++ scripts/vqa/nc_and_minocc_test/vqa_mao-9_normonly-expanded-nc-pt7_default_ft.sh
@@ -13,4 +13,4 @@
 cd ../../..
 source venv/bin/activate
 
-python run.py with norm_clipping=0.7 normonly_flag=expanded data_root=data/vqa_Expanded-nc-gt0.7_occ_gte9_arrow num_gpus=1 num_nodes=1 task_finetune_vqa_clip_bert per_gpu_batchsize=200 load_path=checkpoints/meter_clip16_224_roberta_pretrain.ckpt clip16 text_roberta image_size=224 clip_randaug num_workers=4 loss_type=default vqav2_label_size=147
+python run.py with norm_clipping=0.7 normonly_flag=expanded data_root=data/vqa_Expanded-nc-gt0.7_occ_gte9_arrow num_gpus=1 num_nodes=1 task_finetune_vqa_clip_bert per_gpu_batchsize=200 load_path=checkpoints/meter_clip16_224_roberta_pretrain.ckpt clip16 text_roberta image_size=224 clip_randaug num_workers=4 loss_type=default vqav2_label_size=115
