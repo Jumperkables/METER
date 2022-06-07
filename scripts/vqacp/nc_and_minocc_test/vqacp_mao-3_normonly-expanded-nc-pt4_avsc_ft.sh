dbf32a8771b954046aae3854af1647a7205f0e28
Jumperkables <tom.winterbottom@msn.com>
2022-06-07 11:40:52 +0100

    vqacp scripts fixed to

--- scripts/vqacp/nc_and_minocc_test/vqacp_mao-3_normonly-expanded-nc-pt4_avsc_ft.sh
+++ scripts/vqacp/nc_and_minocc_test/vqacp_mao-3_normonly-expanded-nc-pt4_avsc_ft.sh
@@ -13,4 +13,4 @@
 cd ../../..
 source venv/bin/activate
 
-python run.py with norm_clipping=0.4 normonly_flag=expanded data_root=data/vqacp_Expanded-nc-gt0.4_occ_gte3_arrow num_gpus=1 num_nodes=1 task_finetune_vqa_clip_bert per_gpu_batchsize=200 load_path=checkpoints/meter_clip16_224_roberta_pretrain.ckpt clip16 text_roberta image_size=224 clip_randaug num_workers=4 loss_type=avsc vqav2_label_size=864
+python run.py with norm_clipping=0.4 normonly_flag=expanded data_root=data/vqacp_Expanded-nc-gt0.4_occ_gte3_arrow num_gpus=1 num_nodes=1 task_finetune_vqa_clip_bert per_gpu_batchsize=200 load_path=checkpoints/meter_clip16_224_roberta_pretrain.ckpt clip16 text_roberta image_size=224 clip_randaug num_workers=4 loss_type=avsc vqav2_label_size=698
