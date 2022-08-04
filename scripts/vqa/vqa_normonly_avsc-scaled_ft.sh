a5754a69d16460cf51eeba8b2fe4c8ca09dc9494
Jumperkables <tom.winterbottom@msn.com>
2022-08-04 21:35:32 +0100

    fixing things

--- scripts/vqa/vqa_normonly_avsc-scaled_ft.sh
+++ scripts/vqa/vqa_normonly_avsc-scaled_ft.sh
@@ -5,12 +5,12 @@
 #SBATCH -t 2-00:00
 #SBATCH -x gpu[0-8]
 #SBATCH --qos short
-#SBATCH --job-name vqa_normonly_avsc-scaled_METER
+#SBATCH --job-name vqa_normonly-simlex_avsc-scaled_METER
 #SBATCH --mem 20G
 #SBATCH --gres gpu:1
-#SBATCH -o ../../results/vqa_normonly_avsc-scaled_METER.out
+#SBATCH -o ../../results/vqa_normonly-simlex_avsc-scaled_METER.out
 
 cd ../..
 source venv/bin/activate
 
-python run.py with normonly_flag=True data_root=data/vqa_normonly_arrow num_gpus=1 num_nodes=1 task_finetune_vqa_clip_bert per_gpu_batchsize=200 load_path=checkpoints/meter_clip16_224_roberta_pretrain.ckpt clip16 text_roberta image_size=224 clip_randaug num_workers=4 loss_type=avsc-scaled vqav2_label_size=209
+python run.py with normonly_flag=simlex data_root=data/vqa_normonly_arrow num_gpus=1 num_nodes=1 task_finetune_vqa_clip_bert per_gpu_batchsize=200 load_path=checkpoints/meter_clip16_224_roberta_pretrain.ckpt clip16 text_roberta image_size=224 clip_randaug num_workers=4 loss_type=avsc-scaled vqav2_label_size=209
--- scripts/vqacp/vqacp_normonly_default_ft.sh
+++ scripts/vqacp/vqacp_normonly_default_ft.sh
@@ -5,12 +5,12 @@
 #SBATCH -t 2-00:00
 #SBATCH -x gpu[0-8]
 #SBATCH --qos short
-#SBATCH --job-name vqacp_normonly_default_METER
+#SBATCH --job-name vqacp_normonly-simlex_default_METER
 #SBATCH --mem 20G
 #SBATCH --gres gpu:1
-#SBATCH -o ../../results/vqacp_normonly_default_METER.out
+#SBATCH -o ../../results/vqacp_normonly-simlex_default_METER.out
 
 cd ../..
 source venv/bin/activate
 
-python run.py with normonly_flag=True data_root=data/vqacp_normonly_arrow num_gpus=1 num_nodes=1 task_finetune_vqa_clip_bert per_gpu_batchsize=200 load_path=checkpoints/meter_clip16_224_roberta_pretrain.ckpt clip16 text_roberta image_size=224 clip_randaug num_workers=4 loss_type=default vqav2_label_size=209
+python run.py with normonly_flag=simlex data_root=data/vqacp_normonly_arrow num_gpus=1 num_nodes=1 task_finetune_vqa_clip_bert per_gpu_batchsize=200 load_path=checkpoints/meter_clip16_224_roberta_pretrain.ckpt clip16 text_roberta image_size=224 clip_randaug num_workers=4 loss_type=default vqav2_label_size=209
